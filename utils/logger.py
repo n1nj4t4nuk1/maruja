6fc2e1caf45501a0cf3cf8c22d9275fee04e607b
Javier Parada <javierparada@pm.me>
2026-03-18 11:13:17 +0900

    Add logger utility for structured logging

---
+++ utils/logger.py
@@ -0,0 +1,15 @@
+import logging
+import sys
+
+def setup_logger(name: str = "maruja", level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(level)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        
+    return logger
